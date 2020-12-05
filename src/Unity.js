ec69df84710ace1a9302a4990c5a36896190659b
gursimarsokhi <gursimar.sokhi@gmail.com>
2020-12-05 14:05:34 +0530

    styling the page

--- src/Unity.js
+++ src/Unity.js
@@ -1,9 +1,12 @@
 import Grid from '@material-ui/core/Grid';
 import unity from './unity.json';
 import './content.css';
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const Unity = () => (
+const Unity = () =>{  
+    const currentSlide = () => {
+
+    }
+
+    return(
     <div>
         <Grid container>
             <Grid item xs = {2} className="left">
@@ -16,26 +19,26 @@ const Unity = () => (
                 <div>
                     {unity.sectionTwo.tools.map(
                         (item) => (
-                            <div className = "navBar">
-                                <div className="navBarItem">
-                                <a> {item.category}</a>
-                                </div>
-                            </div>
+                            <ul className = "navBar">
+                                <li className="navBarItem selected">
+                                <a href="#" className="itemLink"> {item.category}</a>
+                                </li>
+                            </ul>
                         ),
                     )}
                 </div>
 
-                <div>
+                <div className="admin">
                     {unity.sectionThree.title}
                 </div>
                 <div>
                     {unity.sectionThree.tools.map(
                         (item) => (
-                            <div>
-                                <div>
-                                 {item.logo} {item.category} {item.count}
-                                </div>
-                            </div>
+                            <ul>
+                                <li className="logoContent">
+                                 <span className="logoText">{item.category}</span> <span className="logoCount">{item.count}</span>
+                                </li>
+                            </ul>
                         ),
                     )}
                 </div>
@@ -44,20 +47,43 @@ const Unity = () => (
                 <div className = "center">
                 <p className = "title">{unity.sectionFive.title}</p>
                 <span className = "subTitle">{unity.sectionFive.subtitle}</span>
+                <a className="closeButton">&times;</a>                
+                <div className = "slideShowContainer">
                         {unity.sectionFive.slider.map(
-                            item => <div className = "centerTopContainer">
+                            item => 
+                                <div className = "centerTopContainer">
+                                <div className="topContent">
                                 <p>{item.title}</p><span>{item.body}</span>
+                                <button>{item.options}</button>
+                                </div>    
                                 <img  className = "centerImage" src={item.imageUrl}/>
+                                <a className="closeButton">&times;</a>
+                                
                                 </div>
                         )}
+                        <span className = "dot"></span>
+                        <span className = "dot"></span>
+                        <span className = "dot"></span>
+                        </div>
+
+                </div>
+                <div className="bottonSection">
+                    {unity.sectionSix.kit.map(
+                        (item) => {return(
+                            <ul className="kitContainer">
+                                <li className="kitList"><img src={item.imageUrl} className="kitImage"/>
+                        <div className="kitContent">{item.title}<div><span className="kitSubContent">{item.subtitle}</span></div></div></li>
+                            </ul>
+                        )}
+                    )}
                 </div>
             </Grid>
             <Grid item xs = {4}>
                 <div>
-
+                
                 </div>
             </Grid>
         </Grid>
     </div>
-)
+)}
 export default Unity
